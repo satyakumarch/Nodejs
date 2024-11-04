747330688c10d24084da2b8147cfab4ef8b9589a
SatyaKumar <satyakumarchaudhary603@gmail.com>
2024-11-05 01:37:42 +0530

    node js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hello");
+// const fs=require('fs');
+// console.log("hello");
+// fs.writeFileSync('hello.txt',"welcome to  nodejs");
+// console.log("directory name-->",__dirname)//print the currrent directory
+// console.log();
+// console.log("filename-->",__filename)//print the file name
+
+const js=require('fs').writeFileSync;
+js('xyz.txt',"welcome to lpu");
