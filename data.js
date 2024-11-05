70db5b8d524c299ff61f3b1c3443b91e6addf645
SatyaKumar <satyakumarchaudhary603@gmail.com>
2024-11-05 22:33:02 +0530

    node js

---
+++ data.js
@@ -0,0 +1,8 @@
+const data=[
+    { id:1,name:"satya",email:"satya@gmail.com"},
+    { id:2,name:"Shyam",email:"shyam@gmail.com"},
+    { id:3,name:"Mohan",email:"mohan@gmail.com"},
+    { id:4,name:"Ram",email:"ram@gmail.com"},
+    {id:5,name:"rahul",email:"rahul@gamil.com"},
+];
+module.exports=data;
--- index.js
+++ index.js
@@ -8,6 +8,38 @@
 // const js=require('fs').writeFileSync;
 // js('xyz.txt',"welcome to lpu");
 
-console.log("satya");
-console.log("hi");
-console.log(10+20);
+// console.log("satya");
+// console.log("hi");
+// console.log(10+20);
+
+
+
+// 11 lecture
+//Simple and basic API
+// const http=require('http');
+// const data=require('./data.js');
+// http.createServer((req,res)=>{
+//     res.writeHead(200, {'Content-Type': 'application/json'});
+//     res.write(JSON.stringify(data));
+//     res.end();
+
+// }).listen(3000);
+
+
+//12 lecture
+//input from command line
+const fs=require('fs');
+const input=process.argv;
+console.log(input);
+if(input[2]=='add'){
+    fs.writeFileSync(input[3],input[4]);  
+}else if(input[2]=='remove')
+    {
+    fs.unlinkSync(input[3]);
+}
+else{
+    console.log('invalid command');
+}
+//code to run this file
+// node index.js remove satya.txt //to remove file
+// node index.js add satya.txt "here is satya kumar chaudhary" //to add file
