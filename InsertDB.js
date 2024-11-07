67c8a385f071385d444fce981f0b8ecf02ce8ae4
SatyaKumar <satyakumarchaudhary603@gmail.com>
2024-11-07 23:50:11 +0530

    fetch data from mongodb and insert it

---
+++ InsertDB.js
@@ -0,0 +1,39 @@
+// const {MongoClient} = require('mongodb');
+// const url = 'mongodb://localhost:27017';
+// const dbname='code-to-code';
+// const client = new MongoClient(url);
+
+// const dbConnect = require("./mongodb");
+
+
+// async function getData()
+// {
+//    let result=await client.connect();
+//    let db=result.db(dbname);
+//    let collection =db.collection('admission');
+// // //    let response=await collection.find({}).toArray();
+// // //    console.log(response);
+
+ 
+
+
+//   const main = async () => {
+//     let data= await dbConnect();
+//      data = await data.find({}).toArray();
+//     console.warn(data);
+// }
+// main();
+// }
+// getData();
+
+
+const dbConnect = require('./mongodb');
+
+const getData = async () => {
+    let db = await dbConnect();
+    let collection = db.collection('admission');
+    let data = await collection.find({}).toArray();
+    console.log(data);
+};
+
+getData();
--- mongodb.js
+++ mongodb.js
@@ -1,15 +1,26 @@
-const {MongoClient} = require('mongodb');
+// const {MongoClient} = require('mongodb');
+// const url = 'mongodb://localhost:27017';
+// const dbname='code-to-code';
+// const client = new MongoClient(url);
+
+// async function dbConnect(){
+//    let result=await client.connect();
+//    let db=result.db(dbname);
+//    let collection =db.collection('admission');
+//    // let response=await collection.find({}).toArray();
+//    // console.log(response);
+// }
+// module.exports = dbConnect;
+
+
+const { MongoClient } = require('mongodb');
 const url = 'mongodb://localhost:27017';
-const dbname='code-to-code';
+const dbname = 'code-to-code';
 
-const client = new MongoClient(url);
+const dbConnect = async () => {
+    const client = new MongoClient(url);
+    await client.connect();
+    return client.db(dbname);
+};
 
-async function getData(){
-   let result=await client.connect();
-   let db=result.db(dbname);
-   let collection =db.collection('admission');
-   let response=await collection.find({}).toArray();
-   console.log(response);
-
-}
-getData();
+module.exports = dbConnect;
