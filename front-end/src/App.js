e6e8cc951f7a62fc0df155428df00a5fd0aa519a
SatyaKumar <satyakumarchaudhary603@gmail.com>
2024-11-16 19:35:16 +0530

    E-commerce

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,29 +1,26 @@
-// import Nav from './Nav.js';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <Nav />
-//       <h1>E-commerce</h1>
-//       <h1>Hello Satya, Welcome to Node.js with React simple project.</h1>
-//     </div>
-//   );
-// }
-
-// export default App;
+// this is the main component of the application
 import React from 'react';
-import Nav from './Nav.js';
+import Nav from './Components/Nav.js';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter ,Routes,Route} from 'react-router-dom';
+import Footer from './Components/Footer.js';
+import Signup from './Components/Signup.js';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Nav />
-        <h1>E-commerce</h1>
+        <Routes>
+          <Route path="/" element={<h1>Product Listing compement</h1>}/>
+          <Route path='/Add' element={<h1>Add Product component</h1>}/>
+          <Route path='/update' element={<h1>update product compement</h1>}/>
+          <Route path='/logout' element={<h1>logout component</h1>}/>
+          <Route path='/profile' element={<h1>profile component</h1>}/>
+          <Route path='/Signup' element={<Signup/>}/>
+        </Routes>
       </BrowserRouter>
+      <Footer/>
     </div>
       
   );
--- front-end/src/Components/Nav.js
+++ front-end/src/Components/Nav.js
@@ -12,6 +12,7 @@ const Nav = () => {
                 <li><Link to ="./update"> Update Product</Link></li>
                 <li><Link to ="./logout"> Logout Product</Link></li>
                 <li><Link to ="./profile">Profile</Link></li>
+                <li><Link to ="./Signup">Signup</Link></li>
 
 
             </ul>
